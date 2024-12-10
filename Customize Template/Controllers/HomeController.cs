b77345138bbb1f140c89d3a0568e0a7243df1267
daleandrew.a <daleandrew.a@BTPITD-L05>
2024-12-10 10:59:39 +0800

    Refine

--- Customize Template/Controllers/HomeController.cs
+++ Customize Template/Controllers/HomeController.cs
@@ -28,6 +28,12 @@ namespace Customize_Template.Controllers
             return View();
         }
 
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Customize Template/Startup.cs
+++ Customize Template/Startup.cs
@@ -50,7 +50,7 @@ namespace Customize_Template
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
